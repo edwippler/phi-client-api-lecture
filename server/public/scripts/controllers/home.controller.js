1e22a89965ba7ee1965dabd58257c19e9b5aaabe
Edmund Wippler <ed.wippler@gmail.com>
2017-03-09 17:34:33 -0600

    displaying available images on the page

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -5,14 +5,35 @@ myApp.controller('HomeController', ['$http', function($http) {
   self.pet = {};
   self.message = "Welcome to the Home View";
 
-  var api_key = "";
+  var api_key = "86523d76eacd7cf0b425bd7800740019";
   var baseURL = "http://api.petfinder.com/";
 
-  self.getRandomPet = function() {
+  self.getRandomDog = function() {
     // create a URL
     var query = baseURL + "pet.getRandom";
     query += "?key=" + api_key;
     query += "&animal=dog";
+    query += "&output=basic";
+    query += "&format=json";
+
+    // console.log('query to API: ', query);
+
+    // encode that URL
+    var request = query + "&callback=JSON_CALLBACK";
+    console.log(request);
+
+    // make ajax request
+    $http.jsonp(request).then(function(response) {
+      console.log('response: ', response.data);
+      self.pet = response.data.petfinder.pet;
+    });
+  }
+  
+  self.getRandomPig = function() {
+    // create a URL
+    var query = baseURL + "pet.getRandom";
+    query += "?key=" + api_key;
+    query += "&animal=pig";
     query += "&output=basic";
     query += "&format=json";
 
